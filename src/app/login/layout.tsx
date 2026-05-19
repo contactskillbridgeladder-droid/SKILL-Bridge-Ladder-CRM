752f6d7a68eeb84467cc55adfb777e4a1479726e
Your GitHub Username <info.krishnanaturals@gmail.com>
2026-05-20 00:31:42 +0530

    fix: remove login layout center-wrapper to make login page truly full screen without side blocks

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default function LoginLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-black font-sans">
-      {children}
-    </div>
-  );
+  return <>{children}</>;
 }
